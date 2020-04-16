bea3e5c4daa249b649b70d887c82790e094874f1
iajordan <sp4c2d0ut@gmail.com>
2020-04-16 01:09:53 -0700

    Completed Part 2

---
+++ Lab4/turnin/ijord001_lab4_part2.c
@@ -0,0 +1,88 @@
+
+/*	Author: Ivannovi Jordan - ijord001@ucr.edu
+ *  Partner(s) Name:
+ *	Lab Section: 28
+ *	Assignment: Lab 4  Exercise 2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States { START, INIT, INC , DEC} state;
+unsigned char tmpA;
+unsigned char count;
+
+void Tick(){
+
+  switch(state){
+    case START:
+      state = INIT;
+      count =0x07;
+      break;
+    case INIT:
+      tmpA = PINA;
+      if(tmpA == 0x01){
+        state = INC;
+      }
+      else if (tmpA == 0x02){
+        state = DEC;
+      }
+      else if (tmpA == 0x03){
+        count = 0x00;
+        state = INIT;
+      }
+      else{
+        state = INIT;
+      }
+      break;
+    case INC:
+      state = INIT;
+      break;
+
+    case DEC:
+      state = INIT;
+      break;
+
+    default:
+      state = START;
+      break;
+  }
+  switch (state){
+    case INIT:
+      PORTC = count;
+      break;
+
+    case INC:
+      if (count < 9){
+        count++;
+      }
+      PORTC = count;
+      break;
+
+    case DEC:
+      if (count > 0){
+        count--;
+      }
+      PORTC = count;
+      break;
+
+    default:
+      break;
+  }
+}
+
+int main(void) {
+  DDRA = 0x00; PORTA = 0xFF;
+  DDRC = 0xFF; PORTC = 0x00;
+
+  tmpA = PINA;
+  count = 0x07;
+  state = START;
+  while (1) { Tick(); }
+  return 1;
+}
